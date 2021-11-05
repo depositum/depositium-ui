1e2d74f5db6e3d354e95111a7b1c5fbc6869d3d2
Dmitry <zeirison@gmail.com>
2021-11-05 22:51:48 +0200

    Update RefContractView

--- src/contract/RefContractView.js
+++ src/contract/RefContractView.js
@@ -1,41 +1,45 @@
-import nearAPI from '../api/NearAPI';
-import walletAPI from '../api/WalletAPI';
+import { nearAPITestnet } from "../api/NearAPI";
+import walletAPI from "../api/WalletAPI";
+import config from "../config";
 
-const view = ({methodName, args = {}}, accountId = 'v2.ref-farming.near') =>
-  nearAPI.connection.provider
+const view = (
+  { methodName, args = {} },
+  accountId = config.refFarmingContractId,
+) =>
+  nearAPITestnet.connection.provider
     .query({
       account_id: accountId,
-      args_base64: Buffer.from(JSON.stringify(args)).toString('base64'),
-      finality: 'final',
+      args_base64: Buffer.from(JSON.stringify(args)).toString("base64"),
+      finality: "final",
       method_name: methodName,
-      request_type: 'call_function',
+      request_type: "call_function",
     })
-    .then(({result}) => JSON.parse(Buffer.from(result).toString()));
+    .then(({ result }) => JSON.parse(Buffer.from(result).toString()));
 
 export const getSeeds = async () =>
   await view({
-    args: {from_index: 0, limit: 100},
-    methodName: 'list_seeds',
+    args: { from_index: 0, limit: 100 },
+    methodName: "list_seeds",
   });
-// v2.ref-farming.near
+
 export const getFarms = async () =>
   await view({
-    args: {from_index: 0, limit: 100},
-    methodName: 'list_farms',
+    args: { from_index: 0, limit: 100 },
+    methodName: "list_farms",
   });
 
 export const getRewardsByAccountId = async ({
   accountId = walletAPI.getAccountId(),
 }) =>
   view({
-    args: {account_id: accountId},
-    methodName: 'list_rewards',
+    args: { account_id: accountId },
+    methodName: "list_rewards",
   });
 
 export const getStakedListByAccountId = async ({
   accountId = walletAPI.getAccountId(),
 }) =>
   await view({
-    args: {account_id: accountId},
-    methodName: 'list_user_seeds',
+    args: { account_id: accountId },
+    methodName: "list_user_seeds",
   });
