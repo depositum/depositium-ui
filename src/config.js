f41b88e267ec40339602b9eafba0d632979b4f72
Dmitry <zeirison@gmail.com>
2021-11-05 22:49:45 +0200

    Update env configs

---
+++ src/config.js
@@ -0,0 +1,14 @@
+export default {
+  depositiumContractId: process.env.REACT_APP_DEPOSITIUM_CONTRACT_ID,
+  explorerUrl: process.env.REACT_APP_EXPLORER_URL,
+  helperUrl: process.env.REACT_APP_HELPER_URL,
+  indexerUrl: process.env.REACT_APP_INDEXER_URL,
+  networkId: process.env.REACT_APP_NETWORK_ID,
+  nodeUrl: process.env.REACT_APP_NODE_URL,
+  refFarmingContractId: process.env.REACT_APP_REF_FARMING_CONTRACT_ID,
+  refFinanceContractId: process.env.REACT_APP_REF_FINANCE_CONTRACT_ID,
+  refNearStrategyContractId:
+    process.env.REACT_APP_REF_NEAR_STRATEGY_CONTRACT_ID,
+  walletUrl: process.env.REACT_APP_WALLET_URL,
+  wrapNearContractId: process.env.REACT_APP_WRAP_NEAR_CONTRACT_ID,
+};
