c632ae90531512a9dbb26765679db6d6d1dcbb86
Dmitry <zeirison@gmail.com>
2021-11-05 21:12:46 +0200

    Remove data preloading from index

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { store } from './store';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { fetchPricesAsync } from './features/providers/priceSlice';
-
-store.dispatch(fetchPricesAsync());
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,7 +12,7 @@ ReactDOM.render(
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root"),
 );
 
 // If you want to start measuring performance in your app, pass a function
